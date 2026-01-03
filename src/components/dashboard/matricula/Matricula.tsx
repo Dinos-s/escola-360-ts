2e3377f9b79cb1088fcaafa01568b0a83b0792f8
Gabriel <gabriel02ramos@gmail.com>
2026-01-03 12:00:39 -0300

    Ajuste na tela de  matrícula

--- src/components/dashboard/matricula/Matricula.tsx
+++ src/components/dashboard/matricula/Matricula.tsx
@@ -1,12 +1,28 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-// import './Matricula.css';
+import './Matricula.css';
+
+type Aluno = {
+  id: number;
+  nome: string;
+};
+
+type Turma = {
+  id: number;
+  nome: string;
+};
+
+type MatriculaType = {
+  id: number;
+  anoLetivo: string;
+  aluno?: Aluno;
+  turma?: Turma;
+};
 
 function Matricula() {
-    const [alunos, setAlunos] = useState<[]>([]);
-    const [turmas, setTurmas] = useState<[]>([]);
-    const [matricula, setMatricula] = useState<[]>([]);
-
+    const [alunos, setAlunos] = useState<Aluno[]>([]);
+    const [turmas, setTurmas] = useState<Turma[]>([]);
+    const [matricula, setMatricula] = useState<MatriculaType[]>([]);
     const [form, setForm] = useState({
         anoLetivo: '',
         alunoId: '',
@@ -14,7 +30,7 @@ function Matricula() {
     });
 
     const [messagem, setMessagem] = useState('');
-    const [tipoMensagem, setTipoMensagem] = useState(''); // 'success' ou 'error'
+    const [tipoMensagem, setTipoMensagem] = useState<'success' | 'error' | ''>(''); // 'success' ou 'error'
 
     useEffect(() => {
         const fetchData = async () => {
@@ -46,11 +62,12 @@ function Matricula() {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         try {
-            await axios.post('http://localhost:3000/matricula', {
+            const res = await axios.post('http://localhost:3000/matricula', {
                 anoLetivo: form.anoLetivo,
                 alunoId: Number(form.alunoId),
                 turmaId: Number(form.turmaId),
             });
+            setMatricula(prev => [...prev, res.data]);
             setMessagem('Matrícula realizada com sucesso!');
             setTipoMensagem('success');
 
@@ -59,7 +76,7 @@ function Matricula() {
                 alunoId: '',
                 turmaId: ''
             });
-        } catch (error: unknown) {
+        } catch (error: any) {
             setMessagem('Erro ao realizar matrícula.');
             setTipoMensagem('error');
         }
@@ -79,10 +96,13 @@ function Matricula() {
                     <div className="form-group">
                         <label>Ano Letivo</label>
                         <input
-                            type="date"
+                            type="number"
                             name="anoLetivo"
                             value={form.anoLetivo}
                             onChange={handleChange}
+                            placeholder="Ex: 2026"
+                            min="1900"
+                            max="2100"
                             required
                         />
                     </div>
@@ -111,12 +131,12 @@ function Matricula() {
                         </select>
                     </div>
 
-                    <button type="submit" className="submit-button">Enviar</button>
+                    <button type="submit" className="save-btn">Enviar</button>
                 </div>
             </form>
 
             {/* TABELA */}
-            <div className="bg-white rounded-xl shadow-sm p-6">
+            {/* <div className="bg-white rounded-xl shadow-sm p-6">
                 <h2 className="text-lg font-semibold text-gray-700 mb-4">
                     Matrículas Cadastradas
                 </h2>
@@ -125,41 +145,38 @@ function Matricula() {
                     <table className="min-w-full text-sm">
                         <thead>
                             <tr className="bg-gray-100 text-gray-600">
-                                <th className="px-4 py-3 text-left font-medium">
+                                <th className="px-4 py-3 text-center font-medium">
                                     Ano Letivo
                                 </th>
-                                <th className="px-4 py-3 text-left font-medium">
+                                <th className="px-4 py-3 text-center font-medium">
                                     Aluno
                                 </th>
-                                <th className="px-4 py-3 text-left font-medium">
+                                <th className="px-4 py-3 text-center font-medium">
                                     Turma
                                 </th>
                             </tr>
                         </thead>
-                        <tbody>                            
+                        <tbody className='divide-y'>
                             {matricula.length === 0 ? (
                                 <tr>
-                                    <td
-                                        colSpan={3}
-                                        className="px-4 py-6 text-center text-gray-500"
-                                    >
+                                    <td colSpan={3} className="px-4 py-6 text-center text-gray-500">
                                         Nenhuma matrícula cadastrada
                                     </td>
                                 </tr>
                             ) : (
-                                matricula.map(matricula => (
+                                matricula.map(m => (
                                     <tr
-                                        key={matricula.id}
+                                        key={m.id}
                                         className="border-b last:border-none hover:bg-gray-50"
                                     >
                                         <td className="px-4 py-3">
-                                            {new Date(matricula.anoLetivo).getFullYear()}
+                                            {new Date(m.anoLetivo).getFullYear()}
                                         </td>
                                         <td className="px-4 py-3">
-                                            {matricula.aluno?.nome}
+                                            {m.aluno?.nome}
                                         </td>
                                         <td className="px-4 py-3">
-                                            {matricula.turma?.nome}
+                                            {m.turma?.nome}
                                         </td>
                                     </tr>
                                 ))
@@ -167,7 +184,36 @@ function Matricula() {
                         </tbody>
                     </table>
                 </div>
-            </div>
+            </div> */}
+
+            <h2 className="table-title">Matrículas Cadastradas</h2>
+
+            <table className="matricula-table">
+                <thead>
+                    <tr>
+                        <th>Ano Letivo</th>
+                        <th>Aluno</th>
+                        <th>Turma</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {matricula.length === 0 ? (
+                        <tr>
+                            <td colSpan={3} style={{ textAlign: "center" }}>
+                                Nenhuma matrícula cadastrada
+                            </td>
+                        </tr>
+                    ) : (
+                        matricula.map(matricula => (
+                            <tr key={matricula.id}>
+                                <td>{new Date(matricula.anoLetivo).getFullYear()}</td>
+                                <td>{matricula.aluno?.nome}</td>
+                                <td>{matricula.turma?.nome}</td>
+                            </tr>
+                        ))
+                    )}
+                </tbody>
+            </table>
         </div>
     )
 }
